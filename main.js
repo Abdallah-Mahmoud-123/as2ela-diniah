ef8b2ea0e215f91b820aff208384604e80b07061
اسئله دينيه <mbody3072@gmail.com>
2025-10-08 08:49:41 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -11,138 +11,138 @@ let on10 = document.getElementById("on10")
 
 
 function showAnswer() {
-    on1.style.color = "black";
-    on2.style.color = "black";
-    on3.style.color = "black";
-    on4.style.color = "black";
-    on5.style.color = "black";
-    on6.style.color = "black";
-    on7.style.color = "black";
-    on8.style.color = "black";
-    on9.style.color = "black";
-    on10.style.color = "black";
+    on1.style.display = "flex";
+    on2.style.display = "flex";
+    on3.style.display = "flex";
+    on4.style.display = "flex";
+    on5.style.display = "flex";
+    on6.style.display = "flex";
+    on7.style.display = "flex";
+    on8.style.display = "flex";
+    on9.style.display = "flex";
+    on10.style.display = "flex";
 
 
 }
 function one1() {
     on1.innerHTML = "<h4>صح</h4><h3>السؤال الاول</h3>";
-    on1.style.color = "white";
+    on1.style.display = "none";
 }
 function one2() {
     on1.innerHTML = "<h4>غلط</h4><h3>السؤال الاول</h3>";
-    on1.style.color = "white";
+    on1.style.display = "none";
 }
 function one3() {
     on1.innerHTML = "<h4>غلط</h4><h3>السؤال الاول</h3>";
-    on1.style.color = "white";
+    on1.style.display = "none";
 }
 function two1() {
     on2.innerHTML = "<h4>صح</h4><h3>السؤال الثاني</h3>";
-    on2.style.color = "white";
+    on2.style.display = "none";
 }
 function two2() {
     on2.innerHTML = "<h4>غلط</h4><h3>السؤال الثاني</h3>";
-    on2.style.color = "white";
+    on2.style.display = "none";
 }
 function two3() {
     on2.innerHTML = "<h4>غلط</h4><h3>السؤال الثاني</h3>";
-    on2.style.color = "white";
+    on2.style.display = "none";
 }
 function three1() {
     on3.innerHTML = "<h4>صح</h4><h3>السؤال الثالث</h3>";
-    on3.style.color = "white";
+    on3.style.display = "none";
 }
 function three2() {
     on3.innerHTML = "<h4>غلط</h4><h3>السؤال الثالث</h3>";
-    on3.style.color = "white";
+    on3.style.display = "none";
 }
 function three3() {
     on3.innerHTML = "<h4>غلط</h4><h3>السؤال الثالث</h3>";
-    on3.style.color = "white";
+    on3.style.display = "none";
 }
 function four1() {
     on4.innerHTML = "<h4>صح</h4><h3>السؤال الرابع</h3>";
-    on4.style.color = "white";
+    on4.style.display = "none";
 }
 function four2() {
     on4.innerHTML = "<h4>غلط</h4><h3>السؤال الرابع</h3>";
-    on4.style.color = "white";
+    on4.style.display = "none";
 }
 function four3() {
     on4.innerHTML = "<h4>غلط</h4><h3>السؤال الرابع</h3>";
-    on4.style.color = "white";
+    on4.style.display = "none";
 }
 function five1() {
     on5.innerHTML = "<h4>صح</h4><h3>السؤال الخامس</h3>";
-    on5.style.color = "white";
+    on5.style.display = "none";
 }
 function five2() {
     on5.innerHTML = "<h4>غلط</h4><h3>السؤال الخامس</h3>";
-    on5.style.color = "white";
+    on5.style.display = "none";
 }
 function five3() {
     on5.innerHTML = "<h4>غلط</h4><h3>السؤال الخامس</h3>";
-    on5.style.color = "white";
+    on5.style.display = "none";
 }
 function six1() {
     on6.innerHTML = "<h4>صح</h4><h3>السؤال السادس</h3>";
-    on6.style.color = "white";
+    on6.style.display = "none";
 }
 function six2() {
     on6.innerHTML = "<h4>غلط</h4><h3>السؤال السادس</h3>";
-    on6.style.color = "white";
+    on6.style.display = "none";
 }
 function six3() {
     on6.innerHTML = "<h4>غلط</h4><h3>السؤال السادس</h3>";
-    on6.style.color = "white";
+    on6.style.display = "none";
 }
 function seven1() {
     on7.innerHTML = "<h4>صح</h4><h3>السؤال السابع</h3>";
-    on7.style.color = "white";
+    on7.style.display = "none";
 }
 function seven2() {
     on7.innerHTML = "<h4>غلط</h4><h3>السؤال السابع</h3>";
-    on7.style.color = "white";
+    on7.style.display = "none";
 }
 function seven3() {
     on7.innerHTML = "<h4>غلط</h4><h3>السؤال السابع</h3>";
-    on7.style.color = "white";
+    on7.style.display = "none";
 }
 function eight1() {
     on8.innerHTML = "<h4>صح</h4><h3>السؤال الثامن</h3>";
-    on8.style.color = "white";
+    on8.style.display = "none";
 }
 function eight2() {
     on8.innerHTML = "<h4>غلط</h4><h3>السؤال الثامن</h3>";
-    on8.style.color = "white";
+    on8.style.display = "none";
 }
 function eight3() {
     on8.innerHTML = "<h4>غلط</h4><h3>السؤال الثامن</h3>";
-    on8.style.color = "white";
+    on8.style.display = "none";
 }
 function nine1() {
     on9.innerHTML = "<h4>صح</h4><h3>السؤال التاسع</h3>";
-    on9.style.color = "white";
+    on9.style.display = "none";
 }
 function nine2() {
     on9.innerHTML = "<h4>غلط</h4><h3>السؤال التاسع</h3>";
-    on9.style.color = "white";
+    on9.style.display = "none";
 }
 function nine3() {
     on9.innerHTML = "<h4>غلط</h4><h3>السؤال التاسع</h3>";
-    on9.style.color = "white";
+    on9.style.display = "none";
 }
 function ten1() {
     on10.innerHTML = "<h4>صح</h4><h3>السؤال العاشر</h3>";
-    on10.style.color = "white";
+    on10.style.display = "none";
 }
 function ten2() {
     on10.innerHTML = "<h4>غلط</h4><h3>السؤال العاشر</h3>";
-    on10.style.color = "white";
+    on10.style.display = "none";
 }
 function ten3() {
     on10.innerHTML = "<h4>غلط</h4><h3>السؤال العاشر</h3>";
-    on10.style.color = "white";
+    on10.style.display = "none";
 }
 function toggleMenu() {
     const menu = document.getElementById('dropdownMenu');
